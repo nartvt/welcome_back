e6fb02d544c4b0b006a167df36d3fcd7a2e5adb7
Tran Van Tai <tooyoutooyou@gmail.com>
2020-03-08 13:23:47 +0700

    welcome back ui-done

--- App.js
+++ App.js
@@ -15,7 +15,9 @@ import {
   Text,
   StatusBar,
   Image,
-  TextInput
+  TextInput,
+  Dimensions,
+  TouchableOpacity
 } from 'react-native';
 
 import {
@@ -26,31 +28,70 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-const App: () => React$Node = () => {
+const widthDevice = Dimensions.get('window').width;
+const heightDevice = Dimensions.get('window').height;
+
+const App = () => {
   return (
     <>
       <View style={{ flex: 1, flexDirection: 'column' }}>
         <View style={{ flex: 1, flexDirection: 'row', backgroundColor: 'white' }}>
           <View style={{ flex: 1, flexDirection: 'row' }}>
-            <Image style={{
-              flex: 1, flexDirection: 'row',
-              width: "100%",
-              height: "100%"
-            }} resizeMode='contain'
-              source={require('./src/assets/img/kids-study.jpg')} />
+            <Image style={{ flex: 1, flexDirection: "row", height: heightDevice / 3 }} resizeMode='stretch' source={require('./src/assets/img/kids-study.jpg')} />
           </View>
         </View>
-        <View style={{ flex: 2, flexDirection: 'row', backgroundColor: 'white', borderRadius: 20 }}>
-          <View style={{ flex: 1, flexDirection: 'column' }}>
-            <View>
-              <Text style={{ color: 'black', fontSize: 40, fontWeight: 'bold', textAlign: 'center' }}> Welcome Back</Text>
-            </View>
-            <View>
-              <Text style={{ color: 'gray', fontSize: 15, fontWeight: 'bold', textAlign: 'center' }}>Login to your account</Text>
-
-            </View>
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-                <TextInput style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}/>
+        <View style={{ flex: 2, flexDirection: 'row', marginTop: '-5%' }}>
+          <View style={{
+            flex: 1, flexDirection: 'column',
+            backgroundColor: 'white',
+            alignItems: 'center',
+            borderRadius: 20
+          }}>
+            <View style={{ flex: 1, alignItems: 'center', marginTop: '10%', }}>
+              <View>
+                <Text style={{ color: 'black', fontSize: 40, fontWeight: 'bold' }}> Welcome Back</Text>
+              </View>
+              <View>
+                <Text style={{ color: 'gray', fontSize: 15, fontWeight: 'bold' }}>Login to your account</Text>
+              </View>
+              <View style={{ marginLeft: '15%', marginTop: '15%' }}>
+                <View>
+                  <TextInput style={styles.textInput}
+                    placeHolderStyle={styles.placeHolderStyle}
+                    underlineColorAndroid="transparent"
+                    placeholder="Email" />
+                </View>
+                <View>
+                  <TextInput style={styles.textInput}
+                    placeHolderStyle={styles.placeHolderStyle}
+                    underlineColorAndroid="transparent"
+                    placeholder="Password"
+                  />
+                </View>
+              </View>
+              <View style={{ marginTop: '15%' }}>
+                <TouchableOpacity>
+                  <View style={{
+                    padding: 15, width: widthDevice / 1.1,
+                    alignItems: 'center',
+                    backgroundColor: '#FF9900',
+                    borderRadius: 10
+                  }}>
+                    <Text>Login</Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+              <View style={{ marginTop: '5%' }}>
+                <Text style={{ fontWeight: 'bold', fontSize: 20, color: 'grey', height: 40 }}>Forgot your password ?</Text>
+              </View>
+              <View style={{ width: widthDevice / 2, marginTop: '15%', flexDirection: 'row' }}>
+                <Text style={{ fontWeight: 'bold', fontSize: 15, color: 'grey', height: 40 }}>
+                  Don't have an account?
+                  </Text>
+                <Text style={{ fontWeight: 'bold', fontSize: 15, color: 'orange', height: 40 }}>
+                  Sign up
+                  </Text>
+              </View>
             </View>
           </View>
         </View>
@@ -60,41 +101,22 @@ const App: () => React$Node = () => {
 };
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+  textInput: {
+    fontSize: 20,
+    height: 40,
+    color: '#1C1C1C',
+    fontWeight: 'bold',
+    borderColor: '#1C1C1C',
+    width: widthDevice,
+    borderRadius: 20,
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
+  viewStyle: {
+    marginLeft: '10%',
+    paddingLeft: '5%',
   },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  placeHolderStyle: {
+    fontFamily: 'Arial',
+    fontSize: 50,
   },
 });
 
